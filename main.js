6f06b056d6db4c6fecdd29c67e75e22a23f91e0c
alsworth <84337775+alsworth@users.noreply.github.com>
2021-06-28 17:50:57 -0400

    added computerPlay function and started working on singleRound function

---
+++ main.js
@@ -0,0 +1,30 @@
+function computerPlay() {
+    let myArray = ['Rock', 'Paper', 'Scissors'];
+    let randomElement = myArray[Math.floor(Math.random() * myArray.length)];
+    return console.log(randomElement);
+}
+
+function singleRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection.toLowerCase();
+    
+    if (playerChoice == 'rock' && computerChoice == 'paper') 
+
+    if (playerChoice == 'paper' && computerChoice == 'scissors') 
+
+    if (playerChoice == 'scissors' && computerChoice == 'rock') 
+
+
+
+
+
+
+
+
+
+
+
+
+    // let message = "The player chose " + playerChoice + " The computer chose " + computerChoice;
+    return message;
+}
