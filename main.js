ffad9ada4f65226e36474b9ca2dc6bd242748c71
alsworth <84337775+alsworth@users.noreply.github.com>
2021-06-29 17:13:10 -0400

    Added singleRound function and started working on game function

--- main.js
+++ main.js
@@ -5,26 +5,45 @@ function computerPlay() {
 }
 
 function singleRound(playerSelection, computerSelection) {
+
     let playerChoice = playerSelection.toLowerCase();
     let computerChoice = computerSelection.toLowerCase();
     
-    if (playerChoice == 'rock' && computerChoice == 'paper') 
+    if (playerChoice == 'rock' && computerChoice == 'paper') {
+        return "You Lose! " + computerChoice + " beats " + playerChoice;
+    }
 
-    if (playerChoice == 'paper' && computerChoice == 'scissors') 
+    else if (playerChoice == 'paper' && computerChoice == 'rock') {
+        return "You Win! " + playerChoice + " beats " + computerChoice;
+    }
 
-    if (playerChoice == 'scissors' && computerChoice == 'rock') 
+    else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        return "You Lose! " + computerChoice + " beats " + playerChoice;
+    }
+
+    else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        return "You Win! " + playerChoice + " beats " + computerChoice;
+    }
+
+    else if (playerSelection == 'scissors' && computerChoice == 'rock') {
+        return "You Lose! " + computerChoice + " beats " + playerChoice;
+    }
+
+    else if (playerSelection == 'rock' && computerChoice == 'scissors') {
+        return "You Win! " + playerChoice + " beats " + computerChoice;
+    }
+
+    else if (playerSelection == computerSelection) {
+        return "TIE";
+    }
+
+}
 
 
-
-
-
-
-
-
-
-
-
-
-    // let message = "The player chose " + playerChoice + " The computer chose " + computerChoice;
-    return message;
+function game() {
+    for (let i = 0; i <= 5; i++) {
+        let playerInput = prompt("Enter your choice: rock, paper or scissors", "rock, paper, scissors");
+        let computerInput = computerPlay();
+        let round = singleRound(userInput, computerInput);
+    }
 }
