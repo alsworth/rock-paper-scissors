34031af467a33a088c6d26b74af3bbfa3bf07749
alsworth <84337775+alsworth@users.noreply.github.com>
2021-07-05 17:47:07 -0400

    added 2 global variable to store computer and player scores and updated playRound() and game() functions

--- main.js
+++ main.js
@@ -5,56 +5,76 @@ function computerPlay() {
     return randomElement;
 }
 
+
+//Creates 2 global variables to store the computer and player scores
+var computerScore = 0;
+var playerScore = 0; 
+
+
 //Displays the results for a 1 round game
 function playRound(playerSelection, computerSelection) {
 
-    let playerChoice = playerSelection
-    let computerChoice = computerSelection
+    let playerChoice = playerSelection;
+    let computerChoice = computerSelection;
 
     playerChoice = playerChoice.toLowerCase();
     computerChoice = computerChoice.toLowerCase();
 
-    let computerScore = 0;
-    let playerScore = 0; 
-    
+
     if (playerSelection == computerSelection) {
         computerScore += 0;
         playerScore += 0;
-        return "TIE";
+        console.log(`TIE
+Player's Score: ${playerScore}
+Computer's Score: ${computerScore}`);      
     }
 
     else if (playerChoice == 'rock' && computerChoice == 'paper') {
         computerScore += 1;
-        return "You Lose! " + computerChoice + " beats " + playerChoice;
+        console.log(`You Lose! ${computerChoice} beats ${playerChoice} 
+Player's Score: ${playerScore}
+Computer's Score: ${computerScore}`);
     }
 
     else if (playerChoice == 'rock' && computerChoice == 'scissors') {
         playerScore += 1;
-        return "You Win! " + playerChoice + " beats " + computerChoice;
+        console.log(`You Win! ${playerChoice} beats ${computerChoice}
+Player's Score: ${playerScore}
+Computer's Score: ${computerScore}`);
     }
 
     else if (playerChoice == 'paper' && computerChoice == 'rock') {
         playerScore += 1;
-        return "You Win! " + playerChoice + " beats " + computerChoice;
+        console.log(`You Win! ${playerChoice} beats ${computerChoice}
+Player's Score: ${playerScore}
+Computer's Score: ${computerScore}`);
     }
 
     else if (playerChoice == 'paper' && computerChoice == 'scissors') {
         computerScore += 1;
-        return "You Lose! " + computerChoice + " beats " + playerChoice;
+        console.log(`You Lose! ${computerChoice} beats ${playerChoice} 
+Player's Score: ${playerScore}
+Computer's Score: ${computerScore}`);
     }
 
     else if (playerSelection == 'scissors' && computerChoice == 'rock') {
         computerScore += 1;
-        return "You Lose! " + computerChoice + " beats " + playerChoice;
+        console.log(`You Lose! ${computerChoice} beats ${playerChoice} 
+Player's Score: ${playerScore}
+Computer's Score: ${computerScore}`);
     }
 
     else if (playerSelection == 'scissors' && computerChoice == 'paper') {
         playerScore += 1;
-        return "You Win! " + playerChoice + " beats " + computerChoice;
+        console.log(`You Win! ${playerChoice} beats ${computerChoice}
+Player's Score: ${playerScore}
+Computer's Score: ${computerScore}`);
+    }
+
+    else {
+        return;
     }
     
-    console.log("The player's score is: " + playerScore + "|| The computer's score is " + computerScore);
-
 }
 
 
@@ -63,8 +83,8 @@ function game() {
     for (let i = 1; i <= 5; i++) {
 
         let computerInput = computerPlay();
-        let userInput = prompt("Enter a choice: rock, paper or scissors");
-        let roundResults = playRound(userInput, computerInput);
+        let playerInput = prompt("Enter a choice: rock, paper or scissors");
+        let roundResults = playRound(playerInput, computerInput);
         console.log(roundResults);
     }
         
